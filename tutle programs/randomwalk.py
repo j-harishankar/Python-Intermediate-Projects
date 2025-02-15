da69bd59a33b1e4bdc9200a4be0157958a7767f8
j harishankar <jharivichu@gmail.com>
2025-02-15 21:37:35 +0530

    test_commit

--- tutle programs/randomwalk.py
+++ tutle programs/randomwalk.py
@@ -3,8 +3,8 @@ from random import *
 timmy = Turtle()
 screen = Screen()
 direction = [0,90,270,180]
-for j in range(100):
+for j in range(200):
     timmy.forward(100)
     timmy.right(choice(direction))
-
+    timmy.left(choice(direction))
 screen.exitonclick()
