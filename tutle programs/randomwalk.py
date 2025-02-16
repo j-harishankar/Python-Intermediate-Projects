f0e7de3676a5137c5f5b26a145a9d530746d5d45
j harishankar <jharivichu27@gmail.com>
2025-02-16 12:25:41 +0530

    final_commit_for_random_walk

--- tutle programs/randomwalk.py
+++ tutle programs/randomwalk.py
@@ -2,8 +2,12 @@ from turtle import *
 from random import *
 timmy = Turtle()
 screen = Screen()
+colors = ["Red","Blue","Gray","Cyan","Magenta","Lime","Maroon","Navy","Olive","Teal","Aqua","Fuchsia","Silver"]
 direction = [0,90,270,180]
+timmy.pensize(15)
+timmy.speed("fastest")
 for j in range(200):
-    timmy.forward(100)
-    timmy.right(choice(direction))
+    timmy.color(choice(colors))
+    timmy.forward(30)
+    timmy.setheading(choice(direction))
 screen.exitonclick()
