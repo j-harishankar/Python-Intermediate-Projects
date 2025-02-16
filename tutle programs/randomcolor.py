2e46ff42bcd4d26b75827c8073c29dc0487e016f
j harishankar <jharivichu27@gmail.com>
2025-02-16 16:32:59 +0530

    randomcolor_turtle_commit

--- tutle programs/randomcolor.py
+++ tutle programs/randomcolor.py
@@ -2,14 +2,21 @@ from turtle import *
 from random import *
 timmy = Turtle()
 screen = Screen()
-colors = ["Red","Blue","Gray","Cyan","Magenta","Lime","Maroon","Navy","Olive","Teal","Aqua","Fuchsia","Silver"]
+colormode(255)
+
+def random_color():
+    r = randint(0,255)
+    g = randint(0,255)
+    b = randint(0,255)
+    return  (r,g,b)
+
+
 direction = [0,90,270,180]
 timmy.pensize(15)
 timmy.speed("fastest")
 for j in range(200):
-    timmy.color(choice(colors))
+    timmy.color(random_color())
     timmy.forward(30)
     timmy.setheading(choice(direction))
 screen.exitonclick()
-
 
