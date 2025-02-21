042a01745b06a6c573f1ae0c79a959fad1df5c64
j harishankar <jharivichu27@gmail.com>
2025-02-21 22:19:18 +0530

    update square.py

--- python beginner project/turlteclass/square.py
+++ python beginner project/turlteclass/square.py
@@ -3,7 +3,7 @@ from turtle import *
 timmy = Turtle()
 screen = Screen()
 for i in range(4):
-    timmy.forward(100)
+    timmy.forward(200)
     timmy.right(90)
-    timmy.forward(100)  # Move the turtle forward by 100 units
+    timmy.forward(200)  # Move the turtle forward by 100 units
 screen.exitonclick()  # Wait for a click to close the window
