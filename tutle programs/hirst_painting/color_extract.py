8eb1c78e10928f2dd64067d95d486435f09eabe7
j harishankar <jharivichu27@gmail.com>
2025-02-21 23:06:56 +0530

    colorgram

--- tutle programs/hirst_painting/color_extract.py
+++ tutle programs/hirst_painting/color_extract.py
@@ -1,15 +1,15 @@
-import colorgram
+#import colorgram
 
 
-def color_ext():
-    rgb_colors = []
-    color = colorgram.extract('image.jpg',50)
-    for i in color:
-        r = i.rgb.r
-        g = i.rgb.g
-        b = i.rgb.b
-        new_color = (r,g,b)
-        rgb_colors.append(new_color)
-    return rgb_colors
-c = color_ext()
-print(c)
+# def color_ext():
+#     rgb_colors = []
+#     color = colorgram.extract('image.jpg',50)
+#     for i in color:
+#         r = i.rgb.r
+#         g = i.rgb.g
+#         b = i.rgb.b
+#         new_color = (r,g,b)
+#         rgb_colors.append(new_color)
+#     return rgb_colors
+# c = color_ext()
+# print(c)
