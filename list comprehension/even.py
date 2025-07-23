61f7ccbfaa8618412b030d363b6a5dec98e1ff93
J Hari Shankar <jharivichu27@gmail.com>
2025-07-23 18:58:57 +0530

    Update even.py

--- list comprehension/even.py
+++ list comprehension/even.py
@@ -6,7 +6,6 @@
 # Then use list comprehension again to create a new list called result.
 # This new list should only contain the even numbers from the list numbers. 
 # Again, try to use Python's List Comprehension instead of a Loop. 
-
 list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
 numbers = [int(numb) for numb in list_of_strings]
 result = [numb for numb in numbers if (numb%2) == 0]
